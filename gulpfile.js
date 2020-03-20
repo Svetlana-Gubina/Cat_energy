16f29dcd54e150f8125f48ebd8de4d66f404e703
Svetlana <gubinas83@gmail.com>
2020-03-20 17:05:19 +0300

    Fix  gulpfile.js, add some classes

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 
 var gulp = require("gulp");
 var plumber = require("gulp-plumber");
-var srcmap = require("gulp-srcmaps");
+var sourcemap = require("gulp-sourcemaps");
 var sass = require("gulp-sass");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
@@ -20,16 +20,16 @@ var uglify = require('gulp-uglify');
 var pipeline = require('readable-stream').pipeline;
 
 gulp.task("css", function () {
-  return gulp.src("src/sass/style.sass")
+  return gulp.src("src/sass/style.scss")
     .pipe(plumber())
-    .pipe(srcmap.init())
+    .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
       autoprefixer()
     ]))
     .pipe(csso())
     .pipe(rename('style.min.css'))
-    .pipe(srcmap.write("."))
+    .pipe(sourcemap.write("."))
     .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
@@ -73,14 +73,15 @@ done();
 gulp.task('images', function() {
   return gulp.src('src/img/**/*.{png,jpg,svg}')
   .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.jpegtran({progressive: true}),
-      imagemin.optipng({optimizationLevel: 3}),
-      imagemin.svgo({
-          plugins: [
-              {cleanupIDs: false}
-          ]
-      })
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.mozjpeg({quality: 75, progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({
+        plugins: [
+            {removeViewBox: true},
+            {cleanupIDs: false}
+        ]
+    })
   ]))
   .pipe(gulp.dest('build/img'));
   });
